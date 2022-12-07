b493dcf9b47636559d56af23773b58045bb2990c
sobatha <natsuki.yokoizo.ny@gmail.com>
2022-12-07 23:55:14 +0900

    feat: qrcodeを生成、pngを返す機能追加
    textで返すとrenderingが面倒なので、qrcodeオブジェクトをcreateで返し、convert pngでpngに変換する

--- api/app/models/qrcode.py
+++ api/app/models/qrcode.py
@@ -4,9 +4,21 @@ URL_CLIENT = "https://localhost:3000/"
 
 
 class Qrcode:
-    def create(str: employee_code) -> str:
+    def create(employee_code) -> str:
         qrcode = pyqrcode.create(
             URL_CLIENT + employee_code, error="Q", version=14
         )  # 400文字のAlphanumetricまでいける
-        qrcode_text = qrcode.text()
-        return qrcode_text
+        return qrcode
+
+    def convert_png(qrcode, file=None):
+        if file is None:
+            return qrcode.png(scale=4)
+        else:
+            return qrcode.png(file, scale=4)
+
+
+if __name__ == "__main__":
+    URL_CLIENT = "https://google.com"
+    employee_code = ""
+    qrcode = Qrcode.create(employee_code)
+    print(Qrcode.convert_png(qrcode, "code.png"))
